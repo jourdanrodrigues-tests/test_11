d2f8d0702d9f2c107fbe88a47db8ce30476ece6f
Thiago Jourdan <thiagojourdan@gmail.com>
2022-11-19 17:22:08 -0300

    :sparkles: Create the "studentController" abstraction

--- src/server/controllers.ts
+++ src/server/controllers.ts
@@ -4,69 +4,70 @@ import { SequelizeErrorHandler, DatabaseErrorHandler } from './errorHandling';
 import { body } from 'express-validator';
 import { hasErrors } from './validation';
 
-export const validateNewStudent = [
-  body('name').isLength({ min: 1, max: 32 }),
-  body('lastName').isLength({ min: 1, max: 32 }),
-  body('email').isEmail().normalizeEmail().isLength({ min: 1 }),
-  body('birthDate').optional({ nullable: true }).isISO8601()
-];
+export const studentController = {
+  validateNewStudent: [
+    body('name').isLength({ min: 1, max: 32 }),
+    body('lastName').isLength({ min: 1, max: 32 }),
+    body('email').isEmail().normalizeEmail().isLength({ min: 1 }),
+    body('birthDate').optional({ nullable: true }).isISO8601()
+  ],
+  validateStudentUpdate: [
+    body('name').optional().isLength({ min: 1, max: 32 }),
+    body('lastName').optional().isLength({ min: 1, max: 32 }),
+    body('birthDate').optional({ nullable: true }).isISO8601()
+  ],
 
-export const validateStudentUpdate = [
-  body('name').optional().isLength({ min: 1, max: 32 }),
-  body('lastName').optional().isLength({ min: 1, max: 32 }),
-  body('birthDate').optional({ nullable: true }).isISO8601()
-];
+  getStudents(req: Request, res: Response): void {
+    Student.findAll().then((students) => res.json(students));
+  },
 
-export function getStudents(req: Request, res: Response): void {
-  Student.findAll().then((students) => res.json(students));
-}
+  findStudent(req: Request, res: Response): void {
+    Student.findByPk(req.params.id)
+      .then((student) => {
+        if (student === null) {
+          res.sendStatus(404);
+        } else {
+          res.json(student);
+        }
+      })
+      .catch(DatabaseErrorHandler(res));
+  },
 
-export function findStudent(req: Request, res: Response): void {
-  Student.findByPk(req.params.id)
-    .then((student) => {
-      if (student === null) {
-        res.sendStatus(404);
-      } else {
-        res.json(student);
-      }
+  addStudent(req: Request, res: Response): void {
+    if (hasErrors(req, res)) return;
+    Student.create(req.body)
+      .then((student) => res.json(student))
+      .catch(SequelizeErrorHandler(res));
+  },
+
+  updateStudent(req: Request, res: Response): void {
+    if (hasErrors(req, res)) return;
+    if (Object.keys(req.body).length === 0) {
+      // No change to be made here
+      this.findStudent(req, res);
+      return;
+    }
+    const studentId = req.params.id;
+    Student.update(req.body, {
+      where: { id: studentId },
+      returning: true
     })
-    .catch(DatabaseErrorHandler(res));
-}
+      .then(([affected, students]) => {
+        if (affected === 0) {
+          res.sendStatus(404);
+        } else {
+          // Since there is an affected row, we can assume it's in this array
+          res.json(students.find(({ id }) => id === studentId));
+        }
+      })
+      .catch(DatabaseErrorHandler(res));
+  },
 
-export function addStudent(req: Request, res: Response): void {
-  if (hasErrors(req, res)) return;
-  Student.create(req.body)
-    .then((student) => res.json(student))
-    .catch(SequelizeErrorHandler(res));
-}
-
-export function updateStudent(req: Request, res: Response): void {
-  if (hasErrors(req, res)) return;
-  if (Object.keys(req.body).length === 0) {
-    // No change to be made here
-    findStudent(req, res);
-    return;
+  deleteStudent(req: Request, res: Response): void {
+    Student.destroy({ where: { id: req.params.id } })
+      .then((affected) => {
+        res.sendStatus(affected === 1 ? 204 : 404);
+      })
+      .catch(DatabaseErrorHandler(res));
   }
-  const studentId = req.params.id;
-  Student.update(req.body, {
-    where: { id: studentId },
-    returning: true
-  })
-    .then(([affected, students]) => {
-      if (affected === 0) {
-        res.sendStatus(404);
-      } else {
-        // Since there is an affected row, we can assume it's in this array
-        res.json(students.find(({ id }) => id === studentId));
-      }
-    })
-    .catch(DatabaseErrorHandler(res));
-}
-
-export function deleteStudent(req: Request, res: Response): void {
-  Student.destroy({ where: { id: req.params.id } })
-    .then((affected) => {
-      res.sendStatus(affected === 1 ? 204 : 404);
-    })
-    .catch(DatabaseErrorHandler(res));
-}
+};
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,16 +1,20 @@
 import express from 'express';
-import * as controller from './controllers';
+import { studentController } from './controllers';
 
 const routes = express.Router();
 
-routes.get('/students', controller.getStudents);
-routes.get('/students/:id', controller.findStudent);
-routes.post('/students', controller.validateNewStudent, controller.addStudent);
+routes.get('/students', studentController.getStudents);
+routes.get('/students/:id', studentController.findStudent);
+routes.post(
+  '/students',
+  studentController.validateNewStudent,
+  studentController.addStudent
+);
 routes.patch(
   '/students/:id',
-  controller.validateStudentUpdate,
-  controller.updateStudent
+  studentController.validateStudentUpdate,
+  studentController.updateStudent
 );
-routes.delete('/students/:id', controller.deleteStudent);
+routes.delete('/students/:id', studentController.deleteStudent);
 
 export default routes;
