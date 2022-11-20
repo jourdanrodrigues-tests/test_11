11c17c78de46acb2e292e1a553a4b260b58a2595
Thiago Jourdan <thiagojourdan@gmail.com>
2022-11-20 17:16:54 -0300

    :tractor: Reorganize GraphQL files

---
+++ src/graphql/index.ts
@@ -0,0 +1,5 @@
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+export const apolloServer = new ApolloServer({ typeDefs, resolvers });
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,25 +1,21 @@
 require('dotenv').config();
 
+import { apolloServer } from '../graphql';
 import express from 'express';
-const { ApolloServer } = require('apollo-server-express');
 import { sequelize } from '../models';
 import routes from './routes';
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
 
 const PORT = process.env.PORT || 3000;
-
-const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 (async () => {
   await sequelize.sync();
-  await server.start();
-  server.applyMiddleware({ app });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
   app.listen(PORT, () => {
-    const message = `Server started on the port ${PORT} (GraphQL at ${server.graphqlPath})`;
+    const message = `Server started on the port ${PORT} (GraphQL at ${apolloServer.graphqlPath})`;
     // eslint-disable-next-line no-console
     console.log(message);
   });
